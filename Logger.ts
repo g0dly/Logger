da32c1f00878219f07d74212739c3e0e6437f0f8
Godly <68485626+g0dly@users.noreply.github.com>
2021-11-03 15:01:28 +1000

    7

--- Logger.ts
+++ Logger.ts
@@ -1,27 +1,81 @@
-class Format {
+interface LoggerOptions {
+  managerOptions?: {
+    format: LoggerFormatManagerOptions;
+  };
+}
 
+class Logger implements LoggerUtil {
+  public formats: LoggerFormatManager;
+
+  constructor(options?: LoggerOptions) {
+    this.formats = new LoggerFormatManager(
+      options && options.managerOptions ? options.managerOptions.format : {}
+    );
+  }
+}
+
+class LoggerUtil {}
+
+interface FormatList {
+  [key: string]: Format;
 }
 
 interface LoggerFormatManagerOptions {
-    importFormats: object[]
+  importFormats?: object[];
 }
 
 class LoggerFormatManager {
-    constructor(options: LoggerFormatManagerOptions) {
+  public defaultFormats: FormatList;
 
-    }
+  constructor(options: LoggerFormatManagerOptions) {
+    this.defaultFormats = {
+      info: new Format({
+        title: "Info",
+        titleColor: "DFDFDF",
+        date: true,
+      }),
+    };
+  }
 }
 
-interface LoggerOptions {
-    managerOptions: {
-        format: LoggerFormatManagerOptions
-    }
+interface ContentOptions {
+  text?: string;
+  stack?: string[];
 }
 
-class Logger {
-    public formats: LoggerFormatManager;
-
-    constructor(options: LoggerOptions) {
-        this.formats = new LoggerFormatManager({ importFormats: options.managerOptions.format.importFormats })
-    }
+interface FormatOptions {
+  title: string;
+  titleColor?: string;
+  textColor?: string;
+  stackColor?: string;
+  date?: boolean;
+  numbered?: boolean;
 }
+
+class Format {
+  private options: FormatOptions;
+  public counter: number;
+
+  constructor(options: FormatOptions) {
+    this.options = options;
+    this.counter = 1;
+  }
+
+  resolve(content: ContentOptions) {
+    let resolved = '';
+
+    resolved += `${this.options.numbered ? `${this.counter}. `: ''}[ ${this.options.title} ] ${this.options.date ? `@${(new Date()).toLocaleString()} ` : ''}"${content.text}"`;
+
+    return resolved
+  }
+
+  test() {
+    console.log(this.options.title);
+  }
+}
+
+//
+
+const log = new Logger();
+
+console.log(log.formats.defaultFormats.info.resolve({ text: 'hi' }));
